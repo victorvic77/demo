ccbc98f5d5e95e647440462fdd8833931646ec90
victorvic77 <60477725+victorvic77@users.noreply.github.com>
2020-03-07 10:32:20 +0300

    Добавление Webpack + Bable

---
+++ src/front/src/index.js
@@ -0,0 +1,33 @@
+import React, { lazy, Suspense } from "react";
+import ReactDOM from "react-dom";
+
+//const App = lazy(() => import("./component/app.component"));
+//ReactDOM.render(
+//<Suspense fallback={<p>loading....</p>}>
+//<App />
+//</Suspense>,
+//document.querySelector("#root")
+//);
+
+
+import App from "./component/app.component";
+ReactDOM.render(<App />, document.querySelector("#root"));
+
+
+
+
+
+// index.js
+//console.log('hello from Node.js')
+
+//const http = require('http')
+//const hostname = '127.0.0.1'
+//const port = 3000
+//const server = http.createServer((req, res) => {
+   // res.statusCode = 200
+   // res.setHeader('Content-Type', 'text/plain')
+   // res.end('Hello Victor. You are Great.\n')
+//})
+//server.listen(port, hostname, () => {
+    //console.log(`Server running at http://${hostname}:${port}/`)
+//})
