5fffb89bd3665e376b65848cd48db8f1852b7d30
victorvic77 <60477725+victorvic77@users.noreply.github.com>
2020-03-07 10:45:26 +0300

    Добавление Webpack + Bable

--- src/front/src/component/app.component.js
+++ src/front/src/component/app.component.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import s from "./app.component.css";
 class MyComponent extends Component {
     render() {
-        return <div className={s.intro}>Hello World</div>;
+        return <div className={s.intro}>Hello Victor. You are Great.</div>;
     }
 }
 export default MyComponent;
