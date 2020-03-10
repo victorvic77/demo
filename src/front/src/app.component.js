0582ad50a7ad36f75bcf58f81dbaedf90cf1092a
victorvic77 <60477725+victorvic77@users.noreply.github.com>
2020-03-10 15:28:30 +0300

    Добавление Webpack + Bable

--- src/front/src/app.component.js
+++ src/front/src/app.component.js
@@ -1,5 +1,47 @@
 import React, { Component } from "react";
 import s from "./app.component.css";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
+
+export default function BasicExample() {
+    return (
+        <Router>
+            <div>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/dashboard">Dashboard</Link>
+                    </li>
+                    /ul>
+
+                    <hr />
+
+                    <Switch>
+                        <Route exact path="/">
+                            <Home />
+                        </Route>
+                        <Route path="/about">
+                            <About />
+                        </Route>
+                        <Route path="/dashboard">
+                            <Dashboard />
+                        </Route>
+                    </Switch>
+            </div>
+        </Router>
+);
+}
+
 
 class MyComponent extends Component {
     render() {
