c3a18fcf3040a6baa13bed8c81ca1052b0a54bc6
victorvic77 <60477725+victorvic77@users.noreply.github.com>
2020-05-02 14:06:46 +0300

    Добавление Webpack + Bable

--- src/front/src/FormContainer.jsx
+++ src/front/src/FormContainer.jsx
@@ -1,23 +1,50 @@
 import React, {Component} from 'react';
+import s from "./app.component.css";
 
 class FormContainer extends React.Component {
+
     constructor(props) {
         super(props);
 
-        this.state = {
-            data: 'Initial data...'
-        }
+        this.state = {data: 'Initial data...'}
+        this.state = {counter: 0}
+       
         this.updateState = this.updateState.bind(this);
+        this.handleClick = this.handleClick.bind(this)
+
     };
     updateState(e) {
+        //debugger;
         this.setState({data: e.target.value});
     }
+    handleClick () {
+        //debugger;
+        console.log("Hello All")
+        this.setState({counter: ++this.state.counter})
+    }
+
     render() {
         return (
             <div>
                 <input type = "text" value = {this.state.data}
                        onChange = {this.updateState} />
                 <h4>{this.state.data}</h4>
+
+                <button className={s.intro}>You are Greate.</button>
+
+                <button className={s.but}>Hello Victor. You are Greate.</button>
+
+                <textarea className={s.text}
+                       placeholder={"Enter your name"}/>
+
+                       <select  className={s.select}></select>
+
+                <h2>{this.state.data}</h2>
+
+                <button onClick = {this.handleClick}>
+                    Click me!(Already Clicked {this.state.counter} times!)
+                </button>
+
             </div>
         );
     }
