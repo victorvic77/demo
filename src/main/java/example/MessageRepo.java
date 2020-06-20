83d7cd3147992b15a201c6150ba8eccedafb685c
victorvic77 <60477725+victorvic77@users.noreply.github.com>
2020-06-20 10:55:04 +0300

    Добавление Webpack + Bable

--- src/main/java/example/MessageRepo.java
+++ src/main/java/example/MessageRepo.java
@@ -7,6 +7,6 @@ import java.util.List;
 
 public interface MessageRepo extends CrudRepository<Message, Long> {
 
-    List<Message> findByTag(String tag);r
+    List<Message> findByTag(String tag);
 
 }
