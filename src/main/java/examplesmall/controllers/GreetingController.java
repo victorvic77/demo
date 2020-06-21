1d6c6b8fdf6a0b41f89a065d273a8d1c789d51ae
victorvic77 <60477725+victorvic77@users.noreply.github.com>
2020-06-21 11:28:04 +0300

    Добавление Webpack + Bable

--- src/main/java/examplesmall/controllers/GreetingController.java
+++ src/main/java/examplesmall/controllers/GreetingController.java
@@ -1,7 +1,7 @@
-package example.controllers;
+package examplesmall.controllers;
 
-import example.domain.Message;
-import example.repos.MessageRepo;
+import examplesmall.domain.Message;
+import examplesmall.repo.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/examplesmall/domain/Message.java
+++ src/main/java/examplesmall/domain/Message.java
@@ -1,11 +1,9 @@
-package example.domain;
+package examplesmall.domain;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
-import javax.persistence.*;
 
 @Entity
 public class Message {
@@ -16,31 +14,12 @@ public class Message {
     private String text;
     private String tag;
 
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "user_id")
-    private User author;
-
-    private String filename;
-
     public Message() {
     }
 
-    public Message(String text, String tag, User user) {
-        this.author = user;
+    public Message(String text, String tag) {
         this.text = text;
         this.tag = tag;
-    }
-
-    public String getAuthorName() {
-        return author != null ? author.getUsername() : "<none>";
-    }
-
-    public User getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(User author) {
-        this.author = author;
     }
 
     public void setText(String text) {
@@ -66,13 +45,4 @@ public class Message {
     public void setTag(String tag) {
         this.tag = tag;
     }
-
-    public String getFilename() {
-        return filename;
-    }
-
-    public void setFilename(String filename) {
-        this.filename = filename;
-    }
 }
-
