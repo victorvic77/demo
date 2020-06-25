c7a0dc4a599607da0485d0c624f4d6526560ef15
victorvic77 <60477725+victorvic77@users.noreply.github.com>
2020-06-25 14:09:25 +0300

    Добавление Webpack + Bable

---
+++ src/main/java/example/domain/Test.java
@@ -0,0 +1,83 @@
+package example.domain;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import javax.persistence.*;
+
+@Entity
+public class Test {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Double Date;
+
+    private Double Time;
+    private Double Open;
+    private Double High;
+    private Double Low;
+    private Double Close;
+    private Double Volume;
+
+    public Test() {
+    }
+
+    public void setDate (Double Date) {
+        this.Date = Date;
+    }
+
+    public Double getDate() {
+        return Date;
+    }
+
+
+    public void setTime (Double Time) {
+        this.Time = Time;
+    }
+
+    public Double getTime() {
+        return Time;
+    }
+
+    public void setHigh (Double High) {
+        this.High = High;
+    }
+
+    public Double getHigh() {
+        return High;
+    }
+
+    public void setOpen (Double Open) {
+        this.Open = Open;
+    }
+
+    public Double getOpen() {
+        return Open;
+    }
+
+    public void setLow (Double Low) {
+        this.Low = Low;
+    }
+
+    public Double getLow() {
+        return Low;
+    }
+
+    public void setClose (Double Close) {
+        this.Close = Close;
+    }
+
+    public Double getClose() {
+        return Close;
+    }
+
+    public void setVolume (Double Volume) {
+        this.Volume = Volume;
+    }
+
+    public Double getVolume() {
+        return Volume;
+    }
+}
